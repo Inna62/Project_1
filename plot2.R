7ab7fce504472d706ee77ef0b09254d63a8cfaf3
Inna <inna.grinina@gmail.com>
2014-12-07 13:41:37 -0500

    Project

---
+++ plot2.R
@@ -0,0 +1,19 @@
+dataset_url <-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(dataset_url, "exdata_data_household_power_consumption.zip")
+directory<-getwd()
+unzip("exdata_data_household_power_consumption.zip", exdir = directory)
+data<-read.table("household_power_consumption.txt", sep=";", header=TRUE)
+data[data=="?"] <- NA
+na.omit(data)
+data[,1] <- as.Date(data[,1], "%d/%m/%Y")
+data1<-data[which(data[,1]=="2007-02-01"),]
+data2<-data[which(data[,1]=="2007-02-02"),]
+data<-rbind(data1, data2)
+data[,3]<-as.numeric(levels(data[,3])[data[,3]])
+dates<-data[,1]
+times<-data[,2]
+x<-paste(dates, times)
+y<-strptime(x, format="%Y-%m-%d %H:%M:%S")
+plot(y, data[,3], type="l", ylab="Global Active Power (kilowatts)", xlab='')
+dev.copy(png,'plot2.png')
+dev.off()
